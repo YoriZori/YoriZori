cf46df94c7a214de457b2b83c32f16309473cbc0
Midan2047 <jeff112630@gmail.com>
2020-05-08 01:21:02 +0900

    moving Turkey(tmp)

--- AndroidApp/app/src/main/java/com/e/yorizori/OpeningActivity.kt
+++ AndroidApp/app/src/main/java/com/e/yorizori/OpeningActivity.kt
@@ -3,8 +3,11 @@ package com.e.yorizori
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginLeft
 
 class OpeningActivity : AppCompatActivity(){
 
@@ -12,14 +15,19 @@ class OpeningActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opening)
         val mProgressBar: ProgressBar
+        val mTurkey: ImageView
         val mCountDownTimer : CountDownTimer
         var i = 0
         mProgressBar = findViewById(R.id.progressBar)
+        mTurkey = findViewById(R.id.Turkey)
         mProgressBar.progress=0
+        val animSlide = AnimationUtils.loadAnimation(applicationContext,R.anim.slide)
+        mTurkey.startAnimation(animSlide)
         mCountDownTimer = object : android.os.CountDownTimer(3000,60){
             override fun onTick(millisUntilFinished: Long){
                 i++
                 mProgressBar.progress = i * 100 / (3000 / 60)
+
             }
 
             override fun onFinish() {
