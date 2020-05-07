c301489f897b117292fd270a573daa79a0ab0d96
Midan2047 <jeff112630@gmail.com>
2020-05-07 22:35:08 +0900

    First commit

---
+++ app/src/main/java/com/e/yorizori/CommunityAcitivity.kt
@@ -0,0 +1,12 @@
+package com.e.yorizori
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CommunityActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
